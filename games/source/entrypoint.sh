96747e10c921e0e2ea877dee722809845823c198
A5R13L <asriel@asriel.dev>
2024-04-18 19:21:04 +0000

    edit(*): FIX

--- games/source/entrypoint.sh
+++ games/source/entrypoint.sh
@@ -43,8 +43,7 @@ else
     echo -e "Not updating game server as auto update was set to 0. Starting Server"
 fi
 
-# Display the command we're running in the output, and then execute it with the env
-# from the container itself.
+# Display the command we're running in the output, and then execute it with the env from the container itself.
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
 # shellcheck disable=SC2086
 exec env ${PARSED}
